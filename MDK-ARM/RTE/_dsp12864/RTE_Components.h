ce7461be35d2df1a9f71664e817516fd9449cefb
Thermit <Ja_P_S@outlook.com>
2020-05-19 23:54:23 +0800

    initial commit

---
+++ MDK-ARM/RTE/_dsp12864/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'dsp12864' 
+ * Target:  'dsp12864' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+#endif /* RTE_COMPONENTS_H */
